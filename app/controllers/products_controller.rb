4d41c386271ec3e2ed15cbc27eea080c0a894f56
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-08-16 04:42:15 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,6 +13,10 @@ class ProductsController < ApplicationController
     render plain: product.description
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def new
     @product = Product.new
   end
@@ -20,6 +24,11 @@ class ProductsController < ApplicationController
   def create
     Product.create(product_params)
     redirect_to products_path
+  end
+
+  def data
+    product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(product)
   end
 
   private
